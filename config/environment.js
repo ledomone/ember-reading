a9619298432d74367c5eaf695c1a9a90793c500c
ledomone <ledomone@users.noreply.github.com>
2016-03-04 22:14:10 +0100

    Add Firebase (Emberfire)

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-reading',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://e-reading.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
